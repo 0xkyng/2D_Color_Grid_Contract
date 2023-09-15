f7c6fcb00daaef59b5e672f5385004197b5be5d7
Isaac <teryima127@gmail.com>
2023-09-15 13:32:56 +0100

    initialization & contract

---
+++ contracts/ColorBoard.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract CellBoard{
+    
+ mapping(uint => mapping(uint => uint)) cells;
+
+ uint randNonce = 0;
+
+ function setColors() public {
+     for(uint x = 0; x < 7; x++){
+         for(uint y = 0; y < 5; y++){
+             randNonce++;
+             if(( x % 2 == 0 ) && ( y % 2 == 0 )){
+                 cells[x][y] = uint(keccak256(abi.encodePacked(block.timestamp,msg.sender,randNonce))) % 4;
+             }
+             if(( x % 2 == 0 ) && ( y % 2 == 1 )){
+                 cells[x][y] = uint(keccak256(abi.encodePacked(block.timestamp,msg.sender,randNonce))) % 4;
+             }
+             if(( x % 2 == 1 ) && ( y % 2 == 0 )){
+                 cells[x][y] = uint(keccak256(abi.encodePacked(block.timestamp,msg.sender,randNonce))) % 4;
+             }
+             if(( x % 2 == 1 ) && ( y % 2 == 1 )){
+                 cells[x][y] = uint(keccak256(abi.encodePacked(block.timestamp,msg.sender,randNonce))) % 4;
+             }
+         }  
+     }
+     
+ }
+
+ function getColor(uint x, uint y) public view returns (uint){
+     require((x < 7) && (y < 5));
+     return cells[x][y];
+ }
+
+}
